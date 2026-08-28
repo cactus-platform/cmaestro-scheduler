03909a45619736a5d467def2499035ee4bd48b8d
qerdinger <91551241+QuentinCraft@users.noreply.github.com>
2026-08-28 07:33:12 +0200

    feat: adds repos management foundation

---
+++ cmaestro-scheduler/internal/bootstrap/collection/repositories.go
@@ -0,0 +1,14 @@
+package collection
+
+import (
+	"context"
+
+	"github.com/cactus-platform/scheduler/internal/config"
+)
+
+type Repositories struct {
+}
+
+func NewRepositories(ctx context.Context, runtimeConfig *config.RuntimeConfig, conns *Connections) (*Repositories, error) {
+	return &Repositories{}, nil
+}
